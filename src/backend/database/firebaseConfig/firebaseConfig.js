64528fa4552e7e3743658c8074f0583ea0443531
Arjun Kumar <98738942+4rjunkum4r@users.noreply.github.com>
2024-03-28 11:47:11 +0530

    Update firebaseConfig.js
    remove my credential which was publicly exposed

--- src/backend/database/firebaseConfig/firebaseConfig.js
+++ src/backend/database/firebaseConfig/firebaseConfig.js
@@ -3,14 +3,7 @@ import { getDatabase } from "firebase/database";
 
 function FirebaseConfig() {
   const firebaseConfig = {
-    apiKey: "AIzaSyDVKVUNYnkvprDnvCHj-HcvfYfPGyi9XtE",
-    authDomain: "onlineprototype-997b6.firebaseapp.com",
-    databaseURL:
-      "https://onlineprototype-997b6-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "onlineprototype-997b6",
-    storageBucket: "onlineprototype-997b6.appspot.com",
-    messagingSenderId: "616104200727",
-    appId: "1:616104200727:web:8ebbb85ab1d2407c216a52",
+    //you can found your credential in your firebase project.
   };
 
   const app = initializeApp(firebaseConfig);
